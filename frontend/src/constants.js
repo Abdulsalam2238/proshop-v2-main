00a8ec963ebfed9ee5c19f74846e399f2edd752c
Abdulsalam2238 <abdulsalamn06@gmail.com>
2026-03-19 16:51:36 +0100

    Update PRODUCTS_URL in constants.js

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,8 +1,1 @@
-
-//process.env.NODE_ENV === 'develeopment' ? 'http://localhost:5000' : '';
-export const PROSHOP_URL = "https://proshop-v2-main-f928.onrender.com/api";
-//export const BASE_URL = ''; // If using proxy
-//export const PRODUCTS_URL = '/api/products';
-//export const USERS_URL = '/api/users';
-//export const ORDERS_URL = '/api/orders';
-//export const PAYPAL_URL = '/api/config/paypal';
+export const PRODUCTS_URL = '/products';
